71359ef54e01b09e395c070115e37bccc4e6e3fa
Sujan Bhattarai <99148876+Sujan-Bhattarai12@users.noreply.github.com>
2023-12-22 09:59:46 -0800

    Update streamlit_app.py
    added breakfast list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
-streamlit.title("my parents healthy diner")
+streamlit.title("My parents healthy diner")
 
-
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 and Blueberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard-Boidled Free-range egg")
